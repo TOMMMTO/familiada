cb46e7c2f72a1d7ddfe5e465c509bcf26fc28d80
tommo <tommowlik21@gmail.com>
2019-09-07 10:32:45 +0200

    Initial commit

---
+++ src/main/java/com/sda/familiada/familiada/FamiliadaApplication.java
@@ -0,0 +1,13 @@
+package com.sda.familiada.familiada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamiliadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamiliadaApplication.class, args);
+    }
+
+}
