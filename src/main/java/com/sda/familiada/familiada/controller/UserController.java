defe4a4f8fb941b81d8c10202a6548a3560e1960
tommo <tommowlik21@gmail.com>
2019-09-08 13:49:45 +0200

    updated missing jsp and corrections

--- src/main/java/com/sda/familiada/familiada/controller/UserController.java
+++ src/main/java/com/sda/familiada/familiada/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.sda.familiada.familiada.controller;
 
 import com.sda.familiada.familiada.model.User;
+import com.sda.familiada.familiada.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/login")
     public ModelAndView getLoginPage() {
@@ -27,6 +36,7 @@ public class UserController {
 
     @PostMapping("/user")
     public String addUser(@ModelAttribute User user) {
+        userService.addUser(user);
         return "redirect:/users";
     }
 
@@ -34,6 +44,14 @@ public class UserController {
     public ModelAndView createUserView() {
         ModelAndView modelAndView = new ModelAndView("addUser");
         modelAndView.addObject("addUser", new User());
+        return modelAndView;
+    }
+
+    @GetMapping("/users")
+    public ModelAndView showAllUsers() {
+        List<User> users = userService.getUserList();
+        ModelAndView modelAndView = new ModelAndView("user");
+        modelAndView.addObject("Users", users);
         return modelAndView;
     }
 
