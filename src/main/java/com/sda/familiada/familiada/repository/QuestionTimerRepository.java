7587db9c3dbd179ad60463da0b1b1f0b764ba5e3
megula22 <47919075+megula22@users.noreply.github.com>
2019-09-14 13:45:52 +0200

    Delete QuestionTimerRepository.java

--- src/main/java/com/sda/familiada/familiada/repository/QuestionTimerRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.sda.familiada.familiada.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface QuestionTimerRepository extends JpaRepository{
-//}
-//
