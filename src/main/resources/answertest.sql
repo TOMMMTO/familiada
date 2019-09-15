c86a1404cdf467be5de8ac47e7189de7261e9356
tommo <tommowlik21@gmail.com>
2019-09-15 15:10:16 +0200

    Merge branch 'master' of C:\Users\tommo\IdeaProjects\familiada with conflicts.

---
+++ src/main/resources/answertest.sql
@@ -0,0 +1,10 @@
+insert INTO QUESTION (id, id_question, answer, points) values (1, 1, 'OWCA', 40);
+insert INTO QUESTION (id, id_question, answer, points) values (2, 1, 'LAMA', 30);
+insert INTO QUESTION (id, id_question, answer, points) values (3, 1,'STADO', 15);
+insert INTO QUESTION (id, id_question, answer, points) values (4, 1,'WATAHA', 8);
+insert INTO QUESTION (id, id_question, answer, points) values (5, 2,'KANAPKA', 36);
+insert INTO QUESTION (id, id_question, answer, points) values (6, 2,'LINIJKA', 30);
+insert INTO QUESTION (id, id_question, answer, points) values (7, 2,'HISTORIA', 10);
+insert INTO QUESTION (id, id_question, answer, points) values (8, 2,'RELIGIA', 1);
+
+commit;
