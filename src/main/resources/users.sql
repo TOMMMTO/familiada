42bdad22e02a221acbefd2c43974b6303502ff7a
TonaroNoTotoro-kun <Lothar55D120>
2019-09-21 11:35:28 +0200

    allelujah!

--- src/main/resources/users.sql
+++
@@ -1,6 +0,0 @@
-insert INTO USER (userId, username) values (nextval('userId_seq'), 'JarczaN');
-insert INTO USER (userId, username) values (nextval('userId_seq'), 'TOMMMTO');
-insert INTO USER (userId, username) values (nextval('userId_seq'), 'megula22');
-insert INTO USER (userId, username) values (nextval('userId_seq'), 'Julita468');
-
-commit;
